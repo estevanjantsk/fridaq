1c070d4ef6f8f046cce2f9ea007387cccea73b35
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-05-25 01:27:16 +0300

    fix a lot of errors

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,67 @@
-console.log("Hello world!");
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { Request, Response, NextFunction } from "express";
+import express from "express";
+
+var createError = require("http-errors");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var sassMiddleware = require("node-sass-middleware");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+const port = 3000;
+
+createConnection()
+  .then((_connection) => {
+    const app = express();
+
+    // view engine setup
+    app.set("views", path.join(__dirname, "views"));
+    app.set("view engine", "pug");
+
+    app.use(logger("dev"));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(
+      sassMiddleware({
+        src: path.join(__dirname, "public"),
+        dest: path.join(__dirname, "public"),
+        indentedSyntax: true, // true = .sass and false = .scss
+        sourceMap: true,
+      })
+    );
+    app.use(express.static(path.join(__dirname, "public")));
+
+    app.use("/", indexRouter);
+    app.use("/users", usersRouter);
+
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+      next(createError(404));
+    });
+
+    // error handler
+    app.use(function (
+      err: any,
+      req: Request,
+      res: Response,
+      _next: NextFunction
+    ) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get("env") === "development" ? err : {};
+
+      // render the error page
+      res.status(err.status || 500);
+      res.render("error");
+    });
+
+    app.listen(port, () =>
+      console.log(`App listening at http://localhost:${port}`)
+    );
+  })
+  .catch((error) => console.log("TypeORM connection error: ", error));
